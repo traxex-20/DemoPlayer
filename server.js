db05da37f774937e0fedac05833bb5ce8aadce30
traxex-20 <69745060+traxex-20@users.noreply.github.com>
2022-08-04 20:22:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ cors_proxy.createServer({
     // 'x-forwarded-proto',
     // 'x-forwarded-port',
   ],
-  redirectSameOrigin: false,
+  redirectSameOrigin: true,
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
