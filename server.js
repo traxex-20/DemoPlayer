c7adf0feaa9d135911484fb2d4c351f8ea3b0865
traxex-20 <69745060+traxex-20@users.noreply.github.com>
2022-08-16 19:49:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: ['origin'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
-    'cookie',
-    'cookie2',
+    //'cookie',
+    //'cookie2',
     // Strip Heroku-specific headers
     'x-request-start',
     'x-request-id',
