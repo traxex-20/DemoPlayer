175f82c6d224853b33c53243f5ee7fdb1d96b0c2
traxex-20 <69745060+traxex-20@users.noreply.github.com>
2022-08-16 19:51:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin'],
+  requireHeader: [],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     //'cookie',
